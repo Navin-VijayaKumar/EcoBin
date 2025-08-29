85b3f4b058c4ae70b5fa769af9038cf04af29d9d
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-08-29 11:21:40 +0530

    Add file upload functionality and update styles
    - Implemented file upload feature in App component
    - Enhanced styling in App.css for better layout and user experience
    - Added new dependencies: react-icon and react-icons

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
   return (
-    <>
-     
-    </>
+    <div className="con">
+      {/* Header */}
+      <div className="title-con">
+        <h1 className='title'><span className='l'>Eco</span><span className='m'>Bin</span></h1>
+      </div>
+
+      {/* Upload Box */}
+      <label className="input-feild">
+        <input 
+          type="file" 
+          onChange={handleChange} 
+          className="file-input"
+        />
+        <FaCloudUploadAlt className="upload-icon" />
+        <p className="upload-text">{file ? file.name : "Upload Image"}</p>
+      </label>
+    </div>
   )
 }
 
